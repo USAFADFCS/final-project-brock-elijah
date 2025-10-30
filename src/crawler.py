fd5a47d00aa7df506e570d91114f37bfb2a1d017
Elijah Sakamoto <esak.code.projects@gmail.com>
2025-10-29 22:00:51 -0600

    added more stuff

--- src/crawler.py
+++ src/crawler.py
@@ -1,17 +1,23 @@
 from core.interfaces.tools import AbstractTool
+import asyncio
+from pyppeteer import launch
 
 
 RATE_LIMITER = 1000 # ms between requests from the same site
 
 
 class CreepyCrawly:
-    def __init__(self, seed):
-        self.to_visit = []
-        self.seed = seed
-        self.to_visit.append(self.seed)
-        self.visited = []
-        self.max_crawl
+    def __init__(self, seed, topic, max_crawl):
+        self.to_visit = [] # the list of unvisited sites that we have discovered
+        self.seed = seed # the url of the original site to start at
+        self.to_visit.append((self.seed, 1)) # each element is a pair of (url, probability of relevance) 
+        self.visited = [] # the list of sites visited
+        self.max_crawl = max_crawl # the max number of sites to visit
+        self.topic = topic # the topic to compare relevance to
+        
+        self.win = 
         
     
     def crawl_next():
-        # pop a 
+        # pop the site off to_visit with the highest likelyhood of being relevant
+        
--- src/research_tool.py
+++ src/research_tool.py
@@ -8,7 +8,12 @@ class Reasearch_Tool(AbstractTool):
     description = (
         "Researches sources across the web for a given topic. Use this ",
         "when you find a topic in the document which is suitable to search",
-        "the web for additional sources on. The input **must** be a "
+        "the web for additional sources on. The input **must** be a brief",
+        "(no more than several words) description of a topic. Expect that",
+        "this tool will keep track of all the sources found for you and will",
+        "only return the total number of sources found up to this point.",
+        "When you finish, these sources will be relayed to the user without",
+        "you seeing them."
     )
     
     
