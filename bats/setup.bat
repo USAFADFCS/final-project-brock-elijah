f4b56fffa8b5fd3f1117fb9eb76cbcff08f01c21
Elijah Sakamoto <esak.code.projects@gmail.com>
2025-11-28 12:57:40 -0700

    added setup script

---
+++ bats/setup.bat
@@ -0,0 +1,10 @@
+python -m venv ./env
+
+call enter.bat
+python -m pip install requests
+python -m pip install openai
+python -m pip install flask
+python -m pip install beautifulsoup4
+
+echo SETUP COMPLETE
+pause
